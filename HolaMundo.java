8eac6bd25a4b8e2626e8e0648b63fed5fe790b72
Jeferson <jbasurco@unsa.edu.pe>
2023-05-07 17:09:32 -0500

    Bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,7 @@ package holamundo;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo!!");
+        
+        System.out.println("Bienvenido al curso!");
     }    
 }
